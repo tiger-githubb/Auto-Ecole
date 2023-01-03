300c7d3fe57c7e0054fadcf6a774fbab4b2bc820
KARBOU Aristide <aristidetiger12@gmail.com>
2023-01-03 02:00:02 +0000

    premier ajout

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Utilisez le composant ici -->
+    <DashboardComponent></DashboardComponent>
+  </div>
+</template>
+
+<script>
+import DashboardComponent from '@/components/DashboardComponent.vue'
+
+export default {
+  components: {
+    DashboardComponent
+  }
+}
+</script>
