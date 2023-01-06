7b9bff8e80260ecaf3ff091799ff4c8d194b2b80
KARBOU Aristide <aristidetiger12@gmail.com>
2023-01-06 00:06:41 +0000

    question read

--- js/getdata.js
+++ js/getdata.js
@@ -1,14 +1,73 @@
 var db = firebase.firestore();
 
-// Récupération de la liste des titres de chapitres de la base de données
-db.collection('chapters').get().then((snapshot) => {
-  snapshot.forEach((doc) => {
-    // Récupération du titre du chapitre à partir du document
-    var chapterTitle = doc.data().title;
-    // Affichage du titre du chapitre sur le site
-    var ul = document.getElementById("chapterList");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(chapterTitle));
-    ul.appendChild(li);
+// affichage des chapitres en tableau
+
+db.collection("chapitres").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const id = doc.data().id;
+    const titre = doc.data().titre;
+    const description = doc.data().description;
+    ajouterLigneTableauChap(id, titre, description);
   });
 });
+
+function ajouterLigneTableauChap(id, titre, description) {
+  const tableau = document.querySelector("#tableau-chapitres");
+  const ligne = document.createElement("tr");
+  ligne.innerHTML = `
+    <td>${id}</td>
+    <td>${titre}</td>
+    <td>${description}</td>
+  `;
+  tableau.appendChild(ligne);
+}
+
+
+// Afficher les questions
+
+db.collection("questions").get().then((querySnapshot) => {
+  querySnapshot.forEach((questionDoc) => {
+    const chapitreId = questionDoc.data().chapitreId;
+    const question = questionDoc.data().texte;
+    const reponse = questionDoc.data().reponse;
+
+    // Récupérer le titre du chapitre
+    db.collection("chapitres").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const chapitre = doc.data().titre;
+        console.log(chapitre);
+
+        // db.collection("chapitres").get().then((querySnapshot) => {
+        //   querySnapshot.forEach((doc) => {
+        //     const id = doc.data().id;
+        //     const titre = doc.data().titre;
+        //     const description = doc.data().description;
+        //     ajouterLigneTableauChap(id, titre, description);
+        //   });
+        // });
+
+        // Récupérer les réponses fausses de la question
+        db.collection("reponses").where("questionId", "==", questionDoc.id).get().then((querySnapshot) => {
+          let reponsesFausses = [];
+          querySnapshot.forEach((reponseDoc) => {
+            reponsesFausses.push(reponseDoc.data().texte);
+          });
+
+          ajouterLigneTableau(chapitre, question, reponse, reponsesFausses);
+        });
+      });
+    });
+  });
+});
+
+function ajouterLigneTableau(chapitre, question, reponse, reponsesFausses) {
+  const tableau = document.querySelector("#tableau-questions");
+  const ligne = document.createElement("tr");
+  ligne.innerHTML = `
+    <td>${chapitre}</td>
+    <td>${question}</td>
+    <td>${reponse}</td>
+    <td>${reponsesFausses.join(", ")}</td>
+  `;
+  tableau.appendChild(ligne);
+}
--- js/senddata.js
+++ js/senddata.js
@@ -1,13 +1,13 @@
 // Référence à la base de données
 var db = firebase.firestore();
 
-const formulaire = document.querySelector("#formulaire-chapitre");
+const formulaire_chap = document.querySelector("#formulaire-chapitre");
 
-formulaire.addEventListener("submit", e => {
+formulaire_chap.addEventListener("submit", e => {
   e.preventDefault();
-  const id = formulaire["id"].value;
-  const titre = formulaire["titre"].value;
-  const description = formulaire["description"].value;
+  const id = formulaire_chap["id"].value;
+  const titre = formulaire_chap["titre"].value;
+  const description = formulaire_chap["description"].value;
   enregistrerChapitre(id, titre, description);
 });
 
@@ -21,32 +21,60 @@ function enregistrerChapitre(id, titre, description) {
     titre: titre,
     description: description
   })
-  .then(function(docRef) {
-    console.log("Chapitre enregistré avec les informations suivantes : ", docRef.id , titre );
-    formulaire.reset(); // vider les champs du formulaire
-  })
-  .catch(function(error) {
-    console.error("Erreur lors de l'enregistrement du chapitre : ", error);
-  });
+    .then(function (docRef) {
+      console.log("Chapitre enregistré avec les informations suivantes : ", docRef.id, titre);
+      formulaire_chap.reset(); // vider les champs du formulaire
+    })
+    .catch(function (error) {
+      console.error("Erreur lors de l'enregistrement du chapitre : ", error);
+    });
 }
 
 
-db.collection("chapitres").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    const id = doc.data().id;
-    const titre = doc.data().titre;
-    const description = doc.data().description;
-    ajouterLigneTableau(id, titre, description);
-  });
+
+// Ajoutet les quetions
+
+const formulaire_quest = document.querySelector("#formulaire-question");
+formulaire_quest.addEventListener("submit", e => {
+  e.preventDefault();
+  const chapitreId = formulaire_quest["chapitreId"].value;
+  const texte = formulaire_quest["texte"].value;
+  const reponse = formulaire_quest["reponse"].value;
+  const reponsesFausses = formulaire_quest["reponsesFausses"].value.split(",");
+  enregistrerQuestion(chapitreId, texte, reponse, reponsesFausses);
 });
 
-function ajouterLigneTableau(id, titre, description) {
-  const tableau = document.querySelector("#tableau-chapitres");
-  const ligne = document.createElement("tr");
-  ligne.innerHTML = `
-    <td>${id}</td>
-    <td>${titre}</td>
-    <td>${description}</td>
-  `;
-  tableau.appendChild(ligne);
+function enregistrerQuestion(chapitreId, texte, reponse, reponsesFausses) {
+  // Initialiser Firebase
+  // ...
+  const db = firebase.firestore();
+
+  // Enregistrer la question
+  db.collection("questions").add({
+    chapitreId: chapitreId,
+    texte: texte,
+    reponse: reponse
+  })
+    .then(function (docRef) {
+      console.log("Question enregistrée avec ID : ", docRef.id);
+
+      // Enregistrer les réponses fausses
+      reponsesFausses.forEach((texteReponseFausse) => {
+        db.collection("reponses").add({
+          questionId: docRef.id,
+          texte: texteReponseFausse
+        })
+          .then(function (docRef) {
+            console.log("Réponse fausse enregistrée avec ID : ", docRef.id);
+            formulaire_quest.reset(); // vider les champs du formulaire
+          })
+          .catch(function (error) {
+            console.error("Erreur lors de l'enregistrement de la réponse fausse : ", error);
+          });
+      });
+    })
+    .catch(function (error) {
+      console.error("Erreur lors de l'enregistrement de la question : ", error);
+    });
 }
+
