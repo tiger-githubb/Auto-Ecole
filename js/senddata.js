be36be17591a7ed36b123239ab30e22f23b61572
KARBOU Aristide <aristidetiger12@gmail.com>
2023-01-08 16:04:29 +0000

    test

--- js/senddata.js
+++ js/senddata.js
@@ -30,6 +30,28 @@ function enregistrerChapitre(id, titre, description) {
     });
 }
 
+// affichage des chapitres en tableau
+
+db.collection("chapitres").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const id = doc.data().id;
+    const titre = doc.data().titre;
+    const description = doc.data().description;
+    ajouterLigneTableauChap(id, titre, description);
+  });
+});
+
+function ajouterLigneTableauChap(id, titre, description) {
+  const tableau = document.querySelector("#tableau-chapitres");
+  const ligne = document.createElement("tr");
+  ligne.innerHTML = `
+    <td>${id}</td>
+    <td>${titre}</td>
+    <td>${description}</td>
+  `;
+  tableau.appendChild(ligne);
+}
+
 
 
 // Ajoutet les quetions
@@ -78,3 +100,47 @@ function enregistrerQuestion(chapitreId, texte, reponse, reponsesFausses) {
     });
 }
 
+// Afficher les questions
+
+db.collection("questions").get().then((querySnapshot) => {
+  querySnapshot.forEach((questionDoc) => {
+    const chapitreId = questionDoc.data().chapitreId;
+    const question = questionDoc.data().texte;
+    const reponse = questionDoc.data().reponse;
+
+    // Récupérer le titre du chapitre correspondant à la question
+    db.collection("chapitres").doc(chapitreId).get().then((chapitreDoc) => {
+      if (chapitreDoc.exists) {
+        // Le document a été trouvé, on peut lire sa propriété "titre"
+        const chapitre = chapitreDoc.data().titre;
+
+        // Récupérer les réponses fausses de la question
+        db.collection("reponses").where("questionId", "==", questionDoc.id).get().then((querySnapshot) => {
+          let reponsesFausses = [];
+          querySnapshot.forEach((reponseDoc) => {
+            reponsesFausses.push(reponseDoc.data().texte);
+          });
+
+          ajouterLigneTableau(chapitre, question, reponse, reponsesFausses);
+        });
+      } else {
+        // Le document n'a pas été trouvé, on peut afficher un message d'erreur ou une valeur par défaut
+        const chapitre = "Chapitre non trouvé";
+        ajouterLigneTableau(chapitre, question, reponse, []);
+      }
+    });
+  });
+});
+
+
+function ajouterLigneTableau(chapitre, question, reponse, reponsesFausses) {
+  const tableau = document.querySelector("#tableau-questions");
+  const ligne = document.createElement("tr");
+  ligne.innerHTML = `
+    <td>${chapitre}</td>
+    <td>${question}</td>
+    <td>${reponse}</td>
+    <td>${reponsesFausses.join(", ")}</td>
+  `;
+  tableau.appendChild(ligne);
+}
